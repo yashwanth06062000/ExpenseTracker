f189b3d0a3330cb5f74ea27f5a55113e96299d32
yashwanth06062000 <yashwanthkrishna150@gmail.com>
2022-03-07 15:58:01 +0530

    SIGn up backend

--- controllers/public.js
+++ controllers/public.js
@@ -1,5 +1,30 @@
-const encript=require('')
+const encript=require('bcryptjs');
 const user=require('../models/usersignup')
 exports.addUser=((req,res,next)=>{
+    const name=req.body.name;
+    const emain=req.body.email;
+    const mobileno=req.body.phoneno
+    const password=req.body.password;
+    const hashpassword= await encript.hash(password,10);
+    const exsitinguser=undefined;
+    user.getusers({where:{name:name,email:emain}})
+    .then((res)=>{
+        existinguser=res;
+
+    })
+    if(exsitinguser != undefined){
+    await user.createUser({name:name,
+    email:emain,
+    phoneno:mobileno,
+    password:hashpassword  }).then((result)=>{
+        console.log(result).catch((err)=>console.log(err))
+        res.status(200).send('SuccesfullyaddedUser')
+    }
+    )}
+    else{
+        res.send("User Already exits")
+        
+    }
+
 
 })
